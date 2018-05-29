e856daf6319cb97fa112b2a9adaabb808fc17f4d
apotnuru <23217476+apotnuru@users.noreply.github.com>
2018-05-29 15:37:13 +0000

    Eclipse paths for c2 build

--- BuilderScripts/eclipse_x86_EXT.sh
+++ BuilderScripts/eclipse_x86_EXT.sh
@@ -1,22 +1,48 @@
 #!/bin/bash -e
 echo HOMEDIR  $HOME
-export WORKSPACE="${HOME}/HBox/EXT/trunk" 
-if [ -f "${HOME}/eclipse/eclipse" ];
-then
-   export ECLIPSEPATH="${HOME}/eclipse"
-   echo "Luna PRESENT"
-else
-	if [ -f "${HOME}/eclipse_Kepler/eclipse" ];
-	then
-	   export ECLIPSEPATH="${HOME}/eclipse_Kepler"
-	   echo "KEPLER PRESENT"
-	else
-	   export ECLIPSEPATH="${HOME}/HBox/eclipse"
-	   echo "JUNO PRESENT"
-	fi
-fi
-echo $PATH
+export EYELOCK_WS_EXT="${HOME}/Anita1/trunk" 
 
-source "${EYELOCK_WS_EXT}/BuilderScripts/setEnv.sh"
+export PKG_PATH="${EYELOCK_WS_EXT}/linux/pkg"
+export TUT_INC=${PKG_PATH}/tut-190-trunk/include
 
-${ECLIPSEPATH}/eclipse -Xmx2048M -data ${WORKSPACE}&
+export STATIC_LIBS=${PKG_PATH}/lib/odroidc2_Release
+export COMMON_INC=${PKG_PATH}/common/include
+
+export EYELOCK_INC=${EYELOCK_WS_EXT}/linux/lib/Include
+export EYELOCK_LIB=${EYELOCK_WS_EXT}/linux/lib/Release
+
+export ALSA_INC=${STATIC_LIBS}
+export ALSA_LIB=${STATIC_LIBS}/alsa/lib
+
+export OPENSSL_INC=${PKG_PATH}/openssl-1.0.1i/include
+export OPENSSL_ASN_INC=${PKG_PATH}/openssl-1.0.1i/crypto/asn1
+export OPENCV_INC=${PKG_PATH}/cv/include
+export CXCORE_INC=${PKG_PATH}/cxcore/include
+export HIGHGUI_INC=${PKG_PATH}/highgui/include
+export APR_INC=${PKG_PATH}/apr-1.4.8/include
+export APR_UTIL_INC=${PKG_PATH}/apr-util-1.5.2/include
+export LOG4CXX_INC=${PKG_PATH}/apache-log4cxx-0.10.0/src/main/include
+
+export THRIFT_BASE=${PKG_PATH}/thrift-0.9.1
+export SQLITE_BASE=${PKG_PATH}/sqlite3
+export SOCI_BASE=${PKG_PATH}/soci-3.2.1
+export BOOST=${PKG_PATH}/boost_1_53_0
+export LIBEVENT=${PKG_PATH}/libevent-2.0.21-stable
+export ZLIB=${PKG_PATH}/zlib-1.2.8
+
+# for MultiChannelLogger
+export POCO_BASE=${STATIC_LIBS}/poco-1.9.0
+export OPENSSL_102N_BASE=${STATIC_LIBS}/openssl-1.0.2o
+
+#for test_aruc
+export FACE_INC=${EYELOCK_WS_EXT}/FaceTracker/facetracker/include
+export CLM_EIGEN_INC=${EYELOCK_WS_EXT}/FaceTracker/CLM/Eigen
+export CLM_INC=${EYELOCK_WS_EXT}/FaceTracker/CLM/include
+export OPENCV_NEW_INC=${STATIC_LIBS}/opencv3.4/include
+export OPENCV_NEW_LIBS=${STATIC_LIBS}/opencv3.4/lib
+
+export ARCH_PROCESSOR="_ARM_"
+
+
+
+eclipse -Xmx2048M -data ${WORKSPACE}&
