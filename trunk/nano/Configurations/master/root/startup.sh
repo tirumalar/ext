a32530a518ba4be2302776a0a966a1b52aa16679
apotnuru <apotnuru@55108f48-39db-479e-846f-0f541c7d32dd>
2018-06-02 01:30:07 +0000

    FaceTracker related changes draft

--- trunk/nano/Configurations/master/root/startup.sh
+++ trunk/nano/Configurations/master/root/startup.sh
@@ -39,11 +39,18 @@ echo ******************************************
 echo running from startup.sh
 echo ******************************************
 
-
 #./flash.sh 
 #./reloadinterfaces.sh
 #/etc/init.d/avahi-daemon restart
 
+# configuring network between COTS and OIM boards
+sysctl -w net.core.rmem_default=3145704
+sysctl -w net.core.rmem_max=3145704
+
+#ifconfig eth0 down
+#ifconfig eth0 hw ether f8:32:e4:9b:39:a2
+#ifconfig eth0 192.168.4.170
+#ifconfig eth0 up
 
 ID=`cat /home/root/id.txt`
 sed -i "s/nano.*-1.local/nanonxt${ID}-1.local/g" /home/root/Eyelock.ini
@@ -51,17 +58,24 @@ sed -i "s/nano.*-0.local/nanonxt${ID}.local/g" /home/root/Eyelock.ini
 
 export NTP_SERVER=`grep -e "GRI\.InternetTimeAddr\=.*" Eyelock.ini | cut -f2 -d'='`
 
-# Only run Eyelock when Eyelock.run exists
 chmod a+x ./icm_communicator
+
+# Only run Eyelock when Eyelock.run exists
 chmod a+x ./Eyelock
 touch Eyelock.run
-#bash -c "while true; do if [ -f /home/root/cvPreview.run ]; then /home/root/cvPreview; fi; sleep 6; done" &
 bash -c "while true; do if [ -f /home/root/Eyelock.run ]; then /home/root/Eyelock; fi; sleep 6; done" &
+
+chmod a+x ./test_aruc
+touch FaceTracker.run
+./test_aruc 8194 1
+#bash -c "while true; do if [ -f /home/root/FaceTracker.run ]; then /home/root/FaceTracker; fi; sleep 6; done" &
+
 #chmod a+x knockd
 #/usr/sbin/knockd -d -c /home/root/knockd.conf 
 #add firewall rules
 #chmod a+x firewall.sh
 #bash -c "while true; do sleep 1; ./firewall.sh; break; done" &
+
 echo "******************************script finished ************************"
 
 
