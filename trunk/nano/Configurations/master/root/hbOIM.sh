cd357ea268a0f49b953a0677e48ffd927f1039f0
apotnuru <apotnuru@55108f48-39db-479e-846f-0f541c7d32dd>
2018-07-11 22:20:30 +0000

    Fixes for OIM recovery script: fixed command for killing the FaceTracker, reodered restarting OIM and interface, increased timeouts (otherwise it's not pinging after recovery). Increased ping requests to prevent false alarms

--- trunk/nano/Configurations/master/root/hbOIM.sh
+++ trunk/nano/Configurations/master/root/hbOIM.sh
@@ -19,7 +19,7 @@ rm "${EYELOCK_RUN_FILE}"
 while true
 do
 	echo "pinging OIM ${OIM_IP}..."
-	if ping -c 1 "${OIM_IP}"
+	if ping -c 5 "${OIM_IP}"
 	then
 		echo "OIM ${OIM_IP} is pingable, sleeping ${TIMEOUT} seconds"
 		touch "${FACETRACKER_RUN_FILE}"
@@ -33,6 +33,7 @@ do
 			if [[ -f ${FLAG} ]]
 			then
 				TIME_SINCE_LAST_RECOVERY=$(($(date +%s) - $(date +%s -r "${FLAG}")))
+				echo "time since last recovery: $TIME_SINCE_LAST_RECOVERY"
 			fi
 
 			if [[ ${TIME_SINCE_LAST_RECOVERY} -ge ${RECOVERY_MIN_INTERVAL} ]] 
@@ -41,28 +42,33 @@ do
 				rm "${EYELOCK_RUN_FILE}"				
 				rm "${FACETRACKER_RUN_FILE}"
 
-				FCTKR_PID=$(ps -ef | grep '/FaceTracker$' | awk '{print $2}')
+				FCTKR_PID=$(ps -ef | grep '/FaceTracker 8194 1$' | awk '{print $2}')
 				if [[ ! -z ${FCTKR_PID} ]]
 				then
+					echo "killing FaceTracker, PID: $FCTKR_PID"
 					kill -9 "${FCTKR_PID}"
 				fi
 
 				ELK_PID=$(ps -ef | grep '/Eyelock$' | awk '{print $2}')
 				if [[ ! -z ${ELK_PID} ]]
 				then
+					echo "killing Eyelock, PID: $ELK_PID"
 					kill -9 "${ELK_PID}"
 				fi
 
-				ifdown eth0
 				sleep 1
 				/home/root/i2cHandler -r0
 				sleep 5
 				/home/root/i2cHandler -r1
+				sleep 30
+				ifdown eth0
 				sleep 5
 				ifup eth0
 
 				touch "${FLAG}"
 				((COUNTER++))
+
+				sleep 10
 			else
 				echo "too soon to recover, skipping..."
 			fi
