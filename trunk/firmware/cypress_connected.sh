5d37e85e36ec9b6e9e151cf805e897215f551cc0
aantonov <aantonov@55108f48-39db-479e-846f-0f541c7d32dd>
2019-05-30 14:09:50 +0000

    IPv6 changes in network script. Fixed subnet prefix length key name

--- trunk/firmware/cypress_connected.sh
+++ trunk/firmware/cypress_connected.sh
@@ -6,7 +6,7 @@ configureStatic(){
 	ifconfig "${IFACE}" hw ether "${MAC}"
 	sleep 1
 	ifconfig "${IFACE}" inet "${IP}" netmask "${MASK}" broadcast "${BRDCAST}" up
-	
+		
 	if [[ ${ENABLED_8021X} == 'true' ]]
 	then
 		# in NXT it is /home/wpa_supplicant
@@ -17,6 +17,9 @@ configureStatic(){
 	#sleep "${WPA_TIMEOUT}"
 
 	route add default gw "${GW}"
+
+	echo "nameserver ${DNS1}" >> "${RESOLV_FILE}"
+	echo "nameserver ${DNS2}" >> "${RESOLV_FILE}"
 }
 
 configureDhcp(){
@@ -36,7 +39,7 @@ configureDhcp(){
 		wpa_supplicant -c /home/www-internal/802.1XCerts/wpa_supplicant-wired.conf -D wired -i "${IFACE}" -ddd -t >> /home/root/wpa_supplicant.log &
 		sleep "${WPA_TIMEOUT}"
 	fi
-
+	
 	bash -c "dhclient -1 -d -v -4 ${IFACE}" &
 }
 
@@ -44,9 +47,12 @@ sleep 5
 echo "$(date +'%Y-%m-%d, %T.000'), INFO , [NetworkConfiguration], - USB-ethernet adapter connected" >> /home/root/nxtLog.log
 
 INTERFACES_FILE='/home/www-internal/interfaces'
+INTERFACES_FILE_6='/home/www-internal/interfaces6'
+RESOLV_FILE='/etc/resolvconf/resolv.conf.d/tail'
 IFACE='usbnet0'
 DHCP_RETRIES=3
 DHCP_TIMEOUT=15
+DHCP6_TIMEOUT=120
 WPA_TIMEOUT=60
 
 MAC="$(grep '^hwaddress ether' ${INTERFACES_FILE} | cut -d' ' -f3)"
@@ -71,13 +77,55 @@ else
 	bash -c "while true; do /home/root/rotateLogs.sh /home/root/wpa_supplicant.log 1000; sleep 10; done" &
 fi
 
+# IPv6
+if grep -q "^EnableIPv6=true" "${INTERFACES_FILE_6}"
+then
+	ENABLED_IPV6='true'
+else
+	ENABLED_IPV6='false'
+fi
+
+DHCP_MODE_6_STR="$(grep '^DhcpMode' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+if [[ ${DHCP_MODE_6_STR} == 'auto' ]]
+then
+	#RECEIVED_ADVERTISEMENTS=$(timeout 15 radvdump -d4) 
+	RADVDUMP_OUT="/tmp/radvdump_out"
+	timeout 15 radvdump -d4 > "${RADVDUMP_OUT}" &
+fi
+
+if grep -q "^Address" "${INTERFACES_FILE_6}"
+then
+	IP6="$(grep '^Address' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+	MASK6="$(grep '^SubnetPrefixLength' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+	GW6="$(grep '^Gateway' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+	DNS16="$(grep '^Dns1' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+	DNS26="$(grep '^Dns2' ${INTERFACES_FILE_6} | cut -d'=' -f2)"
+fi
+
+if [[ ${ENABLED_IPV6} == 'false' ]]
+then
+	sysctl -w net.ipv6.conf.usbnet0.disable_ipv6=1
+else
+	sysctl -w net.ipv6.conf.usbnet0.disable_ipv6=0
+	if grep -q "^AcceptRouterAdvertisements=true" "${INTERFACES_FILE_6}"
+	then
+		sysctl -w net.ipv6.conf.usbnet0.accept_ra=1
+	else
+		sysctl -w net.ipv6.conf.usbnet0.accept_ra=0
+	fi
+fi
+
+echo '' > "${RESOLV_FILE}"
+	
 if grep -q "${IFACE} inet static" "${INTERFACES_FILE}"
 then
 	IP="$(grep '^address' ${INTERFACES_FILE} | cut -d' ' -f2)"
 	MASK="$(grep '^netmask' ${INTERFACES_FILE} | cut -d' ' -f2)"
 	BRDCAST="$(grep '^broadcast' ${INTERFACES_FILE} | cut -d' ' -f2)"
 	GW="$(grep '^gateway' ${INTERFACES_FILE} | cut -d' ' -f2)"
-
+	DNS1="$(grep '^dns-nameservers' ${INTERFACES_FILE} | cut -d' ' -f2)"
+	DNS2="$(grep '^dns-nameservers' ${INTERFACES_FILE} | cut -d' ' -f3)"
+	
 	for (( c=1; c<=3; c++ ))
 	do
 		echo "$(date +'%Y-%m-%d, %T.000'), INFO , [NetworkConfiguration], - configuring static IP ${IP}, netmask ${MASK}, broadcast ${BRDCAST}, gateway ${GW} (attempt ${c})" >> /home/root/nxtLog.log
@@ -120,4 +168,41 @@ then
 		ifconfig "${IFACE}" inet "169.254.1.1" netmask "255.255.0.0" broadcast "169.254.255.255" up
 	fi
 fi
+
+if [[ -n ${IP6} ]]
+then
+	ifconfig "${IFACE}" inet6 add "${IP6}"/"${MASK6}"
+	ip -6 route add "${GW6}" dev "${IFACE}"	
+	echo "nameserver ${DNS16}" >> "${RESOLV_FILE}"
+	echo "nameserver ${DNS26}" >> "${RESOLV_FILE}"
+fi	
+
+if [[ ${DHCP_MODE_6_STR} == 'auto' ]]
+then
+#TODO: check if radvdump is ready
+	RA_MANAGED_FLAG=$(cat "${RADVDUMP_OUT}" | grep 'AdvManagedFlag' | cut -d' ' -f2  | tr -d ';')
+	RA_OTHER_FLAG=$(cat "${RADVDUMP_OUT}" | grep 'AdvOtherConfigFlag' | cut -d' ' -f2  | tr -d ';')
+	rm "${RADVDUMP_OUT}"
+	if [[ ${RA_MANAGED_FLAG} == 'on' ]] 
+	then
+		echo "$(date +'%Y-%m-%d, %T.000'), INFO , [NetworkConfiguration], - Router Advertisements: managed flag is active" >> /home/root/nxtLog.log
+		timeout "${DHCP6_TIMEOUT}" bash -c "dhclient -1 -d -v -6 -N ${IFACE}"
+	elif [[ ${RA_OTHER_FLAG} == 'on' ]] 
+	then
+		echo "$(date +'%Y-%m-%d, %T.000'), INFO , [NetworkConfiguration], - Router Advertisements: other flag is active" >> /home/root/nxtLog.log
+		timeout "${DHCP6_TIMEOUT}" bash -c "dhclient -1 -d -v -6 -S ${IFACE}"
+	fi
+elif [[ ${DHCP_MODE_6_STR} == 'normal' ]]
+then
+	timeout "${DHCP6_TIMEOUT}" bash -c "dhclient -1 -d -v -6 -N ${IFACE}"
+elif [[ ${DHCP_MODE_6_STR} == 'information-only' ]]
+then 
+	timeout "${DHCP6_TIMEOUT}" bash -c "dhclient -1 -d -v -6 -S ${IFACE}"
+else 
+	echo "$(date +'%Y-%m-%d, %T.000'), INFO , [NetworkConfiguration], - IPv6 is disabled" >> /home/root/nxtLog.log
+	# none - IPv6 dhcp disabled
+fi
+
+resolvconf -u
+
 
