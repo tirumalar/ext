c88b708913ad32346bcaea4c2263850b26f4cebe
dopdahl <56830979+dopdahl@users.noreply.github.com>
2020-01-28 19:00:43 +0000

    First sort of working rev of Remote debugging...

--- Internal Tools/ImageLogViewer/ImageLogViewer/ImageLogViewer/Program.cs
+++ Internal Tools/ImageLogViewer/ImageLogViewer/ImageLogViewer/Program.cs
@@ -17,6 +17,22 @@ namespace ImageLogViewer
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainWindow());
+
+
+			#if null
+			MainWindow theWindow = new MainWindow();
+
+            HttpServer httpServer;
+            httpServer = new MyHttpServer(8088, theWindow);
+
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+
+			Application.Run(theWindow);
+
+			httpServer.Kill(1000);
+			thread.Interrupt();
+			#endif
 		}
 	}
 }
